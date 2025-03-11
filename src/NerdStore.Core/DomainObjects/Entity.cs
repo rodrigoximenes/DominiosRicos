146315ee20e0c2c92dba3cfdec5938b086fd2eea
Rodrigo Ximenes <rodrigogaldino@poli.ufrj.br>
2025-03-11 14:41:20 -0300

    Validações

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Entity compareTo) return false;
+            if (ReferenceEquals(this, compareTo)) return true;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (a is null || b is null) return false;
+
+            return a.Id == b.Id;
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
